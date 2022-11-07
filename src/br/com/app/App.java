4680d5f2d74b1081d21c0addbfa550281639ef65
Servidor <marcusrspprodtech@outlook.com>
2022-11-07 17:01:10 -0300

    Implementada a funcionalidade de requisição e resposta HTTP, com leitura e exibição no console do body de resposta e do código de status.

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -1,16 +1,34 @@
 //Declaração do pacote
 package br.com.app;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 //Importação de classe interna
 import br.com.model.KeyReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Instanciação de objeto via construtor padrão KeyReader() e atribuição do valor de referência à variável key
-        KeyReader key = new KeyReader();
-        //Declaração de variável e atribuição do valor de retorno do método .getChave(), contido na classe KeyReader.
-        String chave = key.getChave();
-        //Exibição do valor da variável chave, para conferência
-        System.out.println(chave);
+        //Declaração de variável e atribuição do valor da chave contida no arquivo k_imdb. Usada a classe KeyReader para obtenção do valor da chave de acesso do IMDb API
+        String chave = new KeyReader().getChave();
+
+        //Declaração de variáveis. A variável uri recebe a referência de um objeto instanciado via classe URI. O valor passado no construtor é a concatenação da url de acesso da API ao serviço dos top 250 filmes e o valor da chave de acesso
+        String url = "https://imdb-api.com/en/API/Top250Movies/" + chave;
+        URI uri = new URI(url);
+
+        //Criação de fluxo de requição e reposta via HTTP. Instanciament de client, construção da requisição e leitura do corpo da resposta
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        //Armazenamento do código de status da resposta, assim como o corpo da resposta. No caso um JSON.
+        int statusCode = response.statusCode();
+        String conteudo = response.body();
+
+        //Exibição dos valores contidos nas variáveis para conferência
+        System.out.println(conteudo);
+        System.out.println("Status code: " + statusCode);
     }
 }
