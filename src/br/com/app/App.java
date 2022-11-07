c66715cb1457027e8b555d7c25d0a98ba1e0a9b3
Servidor <marcusrspprodtech@outlook.com>
2022-11-07 16:24:40 -0300

    Criação de comentários

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -6,8 +6,11 @@ import br.com.model.KeyReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        //Instanciação de objeto via construtor padrão KeyReader() e atribuição do valor de referência à variável key
         KeyReader key = new KeyReader();
+        //Declaração de variável e atribuição do valor de retorno do método .getChave(), contido na classe KeyReader.
         String chave = key.getChave();
+        //Exibição do valor da variável chave, para conferência
         System.out.println(chave);
     }
 }
