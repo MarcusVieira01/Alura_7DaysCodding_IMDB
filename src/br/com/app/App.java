c01b6a6821be48b2e0aa0874af6d12f6005a3529
Servidor <marcusrspprodtech@outlook.com>
2022-11-13 20:57:06 -0300

    Implementação de lista com objetos do tipo Movie, recebendo os parâmetros das listas title, year, rating e image

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -2,49 +2,41 @@
 package br.com.app;
 
 //Importação de calsse externa
+import java.util.ArrayList;
 import java.util.List;
 import br.com.model.HttpRequestIMDB;
+import br.com.model.Movie;
 
 //Importação de classe interna
 import br.com.model.ParseJsonIMDB;
 
-
+//Declaração de classe
 public class App {
     public static void main(String[] args) throws Exception {
         //Instanciação de objeto da classe ParseJsonIMDB passando como parâmetro o corpo da requisição HTTP, realizada via instanciamento da classe HttpRequestIMDB com aninhamento do método que fará o retorno do corpo da requisição no tipo String
         ParseJsonIMDB json = new ParseJsonIMDB(new HttpRequestIMDB().request());
+        
+        //Instancição de novo objeto via construtor ArrayList<>()
+        List<Movie> filmes = new ArrayList<>();
 
         //Declaração de variáveis e atribuição dos valores de retorno dos métodos evocados.  
-        List<String> id = json.parseID();
         List<String> title = json.parseTitulo();
-        List<String> rank = json.parseRank();
+        List<String> year = json.parseYear();
+        List<String> rating = json.parseRating();
         List<String> image = json.parseElemento("image");
 
-      
+        //Loop for que fará a adição de elemento no objeto de referência atribuída à variável filmes. Cada elemento será um objeto instanciado via construtor Movie(args)
+        for(int i = 0; i < title.size(); i++){
+            filmes.add(new Movie(title.get(i), rating.get(i), year.get(i), image.get(i)));
+        }
 
 
 
 
 
-        //USO PARA DEBUG MANUAL ____________________
 
-        //Exibição do elemento de valor da variável indice para os parâmentros desejados, como um teste de retorno
-        int indice = 3;
-        System.out.println("Elemento " + indice + " tem:\n" + 
-        "id " + id.get(indice) + "\n" + 
-        "titulo " + title.get(indice) + "\n" + 
-        "rank " + rank.get(indice) + "\n" + 
-        "image " + image.get(indice) + "\n");
 
-        //Uso de iteração via método forEach() para exibir cada elemento do arraylist titulo, para conferência
-        // id.forEach(System.out::println);
-        // title.forEach(System.out::println);
-        // rank.forEach(System.out::println);
-        // image.forEach(System.out::println);
-        //Exibiçãod o retorno do método que trará a quantidade de elementos do arraylist titulos
-        // System.out.println("Elementos: " + title.size());
-        //Exibição do valor da variável statuscode, que armazena o status code da requisição HTTP
-        // System.out.println("Status code: " + response.statusCode());
-
+        //Exibição de resultado para teste
+        System.out.println(filmes.get(3));
     }
 }
