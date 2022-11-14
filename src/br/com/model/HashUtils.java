06cef8bdc91a0c7f66218d388011412fc22111fd
Servidor <marcusrspprodtech@outlook.com>
2022-11-14 15:38:05 -0300

    Criação de classe que fará a elaboração de um hash conforme algorítmo MD5

---
+++ src/br/com/model/HashUtils.java
@@ -0,0 +1,13 @@
+package br.com.model;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtils {
+    public static String getHashMd5(String value) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        BigInteger hash = new BigInteger(1, md.digest(value.getBytes()));
+        return hash.toString(16);
+    }
+}
