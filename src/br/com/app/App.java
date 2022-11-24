cc45a32e470563f239968d1d437bc6840f69f313
Servidor <marcusrspprodtech@outlook.com>
2022-11-24 18:51:19 -0300

    Atualização para separação entre testes IMDb API e API Marvel

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -1,8 +1,9 @@
+//Execução pronta para IMDb
+
 //Declaração do pacote
 package br.com.app;
 
 //Importação de calsse externa
-//import java.io.BufferedWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,26 +12,26 @@ import java.util.List;
 import br.com.model.ParseJsonIMDB;
 import br.com.model.Movie;
 import br.com.model.GeradorHtml;
-import br.com.model.HttpRequestIMDB;
+import br.com.model.HttpRequestApi;
 
 //Declaração de classe
 public class App {
     public static void main(String[] args) throws Exception {
         //Instanciação de objeto da classe ParseJsonIMDB passando como parâmetro o corpo da requisição HTTP, realizada via instanciamento da classe HttpRequestIMDB com aninhamento do método que fará o retorno do corpo da requisição no tipo String
-        ParseJsonIMDB json = new ParseJsonIMDB(new HttpRequestIMDB().request());
+        ParseJsonIMDB jsonImdb = new ParseJsonIMDB(new HttpRequestApi().requestImdb("k_imdb.txt"));
         
         //Instancição de novo objeto via construtor ArrayList<>()
-        List<Movie> filmes = new ArrayList<>();
+        List<Movie> filmesImdb = new ArrayList<>();
 
         //Declaração de variáveis e atribuição dos valores de retorno dos métodos evocados.  
-        List<String> title = json.parseTitulo();
-        List<String> year = json.parseYear();
-        List<String> rating = json.parseRating();
-        List<String> image = json.parseElemento("image");
+        List<String> title = jsonImdb.parseTitulo();
+        List<String> year = jsonImdb.parseYear();
+        List<String> rating = jsonImdb.parseRating();
+        List<String> image = jsonImdb.parseElemento("image");
 
         //Loop for que fará a adição de elemento no objeto de referência atribuída à variável filmes. Cada elemento será um objeto instanciado via construtor Movie(args)
         for(int i = 0; i < title.size(); i++){
-            filmes.add(new Movie(title.get(i), rating.get(i), year.get(i), image.get(i)));
+            filmesImdb.add(new Movie(title.get(i), rating.get(i), year.get(i), image.get(i)));
         }
 
         //Instanciação de objeto Writer via construtor PrintWriter(arquivo), que fará a escrita do conteúdo gerado via classe GeradorHtml
@@ -40,7 +41,7 @@ public class App {
         GeradorHtml html = new GeradorHtml(writer);
 
         //Evocação de método para geração do código e arquivo HTML e passando o objeto List filmes, que contém os objetos Movie e seus atributos. Evocado método para fechamento do acesso ao objeto writer
-        html.gerarHtml(filmes);      
+        html.gerarHtml(filmesImdb);      
         writer.close();
 
         //Exibição de mensagem que indica finalização da execução do programa
