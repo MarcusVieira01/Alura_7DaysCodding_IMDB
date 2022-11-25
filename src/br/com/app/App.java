960f3a8705335bb990f26add6ee44d418b5d2d55
Servidor <marcusrspprodtech@outlook.com>
2022-11-25 19:55:00 -0300

    Remoção de porção de código para especializar a classe App apenas com solicitações de métodos. Trecho incluído na classe Movie. Dessa forma a lista de objetos Movie é gerada pela própria classe

--- src/br/com/app/App.java
+++ src/br/com/app/App.java
@@ -5,8 +5,6 @@ package br.com.app;
 
 //Importação de calsse externa
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 //Importação de classe interna
 import br.com.model.ParseJsonIMDB;
@@ -20,20 +18,6 @@ public class App {
         //Instanciação de objeto da classe ParseJsonIMDB passando como parâmetro o corpo da requisição HTTP, realizada via instanciamento da classe HttpRequestIMDB com aninhamento do método que fará o retorno do corpo da requisição no tipo String
         ParseJsonIMDB jsonImdb = new ParseJsonIMDB(new HttpRequestApi().requestImdb("k_imdb.txt"));
         
-        //Instancição de novo objeto via construtor ArrayList<>()
-        List<Movie> filmesImdb = new ArrayList<>();
-
-        //Declaração de variáveis e atribuição dos valores de retorno dos métodos evocados.  
-        List<String> title = jsonImdb.parseTitulo();
-        List<String> year = jsonImdb.parseYear();
-        List<String> rating = jsonImdb.parseRating();
-        List<String> image = jsonImdb.parseElemento("image");
-
-        //Loop for que fará a adição de elemento no objeto de referência atribuída à variável filmes. Cada elemento será um objeto instanciado via construtor Movie(args)
-        for(int i = 0; i < title.size(); i++){
-            filmesImdb.add(new Movie(title.get(i), rating.get(i), year.get(i), image.get(i)));
-        }
-
         //Instanciação de objeto Writer via construtor PrintWriter(arquivo), que fará a escrita do conteúdo gerado via classe GeradorHtml
         PrintWriter writer = new PrintWriter("index.html");
 
@@ -41,7 +25,7 @@ public class App {
         GeradorHtml html = new GeradorHtml(writer);
 
         //Evocação de método para geração do código e arquivo HTML e passando o objeto List filmes, que contém os objetos Movie e seus atributos. Evocado método para fechamento do acesso ao objeto writer
-        html.gerarHtml(filmesImdb);      
+        html.gerarHtml(Movie.gerarListaFilmes(jsonImdb));      
         writer.close();
 
         //Exibição de mensagem que indica finalização da execução do programa
