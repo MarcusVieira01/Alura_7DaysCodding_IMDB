3f5e435902f7bc2ae4cf2aa9d60a09b28d972959
Servidor <marcusrspprodtech@outlook.com>
2022-11-25 17:54:39 -0300

    Criação de classe que fará abstração dos objetos que a implementem e permita generalização da lista de objetos passada como argumento para a classe GeradorHTML

---
+++ src/br/com/model/Content.java
@@ -0,0 +1,11 @@
+//Declaração de pacote
+package br.com.model;
+
+//Declaração de interface
+public interface Content {
+    //Declaração de métodos obrigatórios
+    public String getTitle();
+    public float getRating();
+    public int getYear();
+    public String getImage();
+}
