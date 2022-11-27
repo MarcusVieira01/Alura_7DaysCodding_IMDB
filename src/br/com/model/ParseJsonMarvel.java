8e911f10f2386704f4c24ba79a6fd3273be88341
Servidor <marcusrspprodtech@outlook.com>
2022-11-26 21:04:20 -0300

    Implementação dos parseamentos dos elementosdos demais elementos

--- src/br/com/model/ParseJsonMarvel.java
+++ src/br/com/model/ParseJsonMarvel.java
@@ -1,11 +1,9 @@
 //Declaração de pacote
 package br.com.model;
 
+//Importação de classes externas
 import java.util.ArrayList;
 import java.util.List;
-
-//Importação de classes externas
-
 
 /**
  *@apiNote Declaração de classe que fará o parseamento do JSON de retorno da API Marvel
@@ -34,7 +32,7 @@ public class ParseJsonMarvel {
      * Método que fará a seccção do valor do atributo json em elementos individuais para posterior parseamento
      * @return Array de strings com os elementos seccionados do JSON de resposta da API Marvel
      */
-    public String[] extrairJson(){
+    private String[] extrairJson(){
         //Declraação de arrays e atribuição dos retornos dos métodos .split(), onde é separado cada elemento do JSON
         String[] primeiroSplit = json.split(",\"results\":");
         String[] segundoSplit = primeiroSplit[1].split("]}}");
@@ -44,15 +42,16 @@ public class ParseJsonMarvel {
     }
 
     /**
-     * 
-     * @return
+     * Método de parseamento do elemento title do atributo JSON
+     * @return Lista de String com os elementos parseados
      */
     public List<String> parseTitle(){
-        //
+        //Declaração de variável lista via construtor, onde serão armazenados os elementos parseados
         List<String> listaTitle = new ArrayList<>();
-        //
+
+        //Template foreach que fará a iteração do array retornado pelo método interno extrairJson()
         for (String elemento : extrairJson()) {
-            //
+            //Condicional que incluirá o elemento do array caso contenha title, após remoção de caracteres indesejados 
             if(elemento.contains("title\":\" ")){
                 listaTitle.add(elemento.replaceAll("title\":\" ", "").replaceAll("\"", ""));
             }else if(elemento.contains("title\":\"")){
@@ -60,6 +59,68 @@ public class ParseJsonMarvel {
             }
         }
 
+        //Retorno do objeto lista
         return listaTitle;
     }
+
+    /**
+     * Método de parseamento do elemento rating do atributo JSON
+     * @return Lista de String com os elementos parseados
+     */
+    public List<String> parseRating(){
+        //Declaração de variável lista via construtor, onde serão armazenados os elementos parseados
+        List<String> listaRating = new ArrayList<>();
+
+        //Template foreach que fará a iteração do array retornado pelo método interno extrairJson()
+        for (String elemento : extrairJson()) {
+            //Condicional que incluirá o elemento do array caso contenha rating, após remoção de caracteres indesejados 
+            if(elemento.contains("rating\":")){
+                listaRating.add(elemento.replaceAll("rating\":", ""));
+            }
+        }
+
+        //Retorno do objeto lista
+        return listaRating;
+    }
+
+    /**
+     * Método de parseamento do elemento year do atributo JSON
+     * @return Lista de String com os elementos parseados
+     */
+    public List<String> parseYear(){
+        //Declaração de variável lista via construtor, onde serão armazenados os elementos parseados
+        List<String> listaYear = new ArrayList<>();
+
+        //Template foreach que fará a iteração do array retornado pelo método interno extrairJson()
+        for (String elemento : extrairJson()) {
+            //Condicional que incluirá o elemento do array caso contenha rating, após remoção de caracteres indesejados 
+            if(elemento.contains("startYear\":")){
+                listaYear.add(elemento.replaceAll("startYear\":", ""));
+            }
+        }
+
+        //Retorno do objeto lista
+        return listaYear;
+    }
+
+    /**
+     * Método de parseamento do elemento image do atributo JSON
+     * @return Lista de String com os elementos parseados
+     */
+    public List<String> parseImage(){
+        //Declaração de variável lista via construtor, onde serão armazenados os elementos parseados
+        List<String> listaImage = new ArrayList<>();
+
+        //Template foreach que fará a iteração do array retornado pelo método interno extrairJson()
+        for (String elemento : extrairJson()) {
+            //Condicional que incluirá o elemento do array caso contenha rating, após remoção de caracteres indesejados 
+            if(elemento.contains("thumbnail\":")){
+                listaImage.add(elemento.replaceAll("thumbnail\":\\{\"path\":\"", "").replaceFirst("\"", "") + ".jpg");
+            }
+        }
+
+        //Retorno do objeto lista
+        return listaImage;
+    }
+
 }
