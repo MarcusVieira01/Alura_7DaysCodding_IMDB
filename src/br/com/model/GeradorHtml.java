34595530b8285a2a400a796864c21fd160afc6bc
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-04 17:21:03 -0300

    Atualização dos conteúdos dos trechos finais das gerações de HTML para que o bootstrap funcione corretamente

--- src/br/com/model/GeradorHtml.java
+++ src/br/com/model/GeradorHtml.java
@@ -36,7 +36,8 @@ public class GeradorHtml {
                 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
 
                 <!-- CSS only -->
-                <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi\" crossorigin=\"anonymous\">  
+                <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" 
+                integrity=\"sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi\" crossorigin=\"anonymous\">  
 
                 <title>Lista TOP 250 - IMDb API</title>
             </head>
@@ -76,7 +77,9 @@ public class GeradorHtml {
         //Escrita do fim do arquivo HTML
         this.escritor.write("""
                         </div>
-                    </body>
+                        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" 
+                        integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>
+                    </body> 
                 </html>
                 """);
     }
@@ -132,6 +135,8 @@ public class GeradorHtml {
         //Escrita do fim do arquivo HTML
         this.escritor.write("""
                         </div>
+                        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" 
+                        integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>
                     </body>
                 </html>
                 """);
